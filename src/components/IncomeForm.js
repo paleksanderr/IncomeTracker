e2bcf78634f33a95603864e20ba6ba22ec4afbdd
Pawel <pawel.aleksander@gmail.com>
2022-06-23 16:28:41 +0200

    adding progres

--- src/components/IncomeForm.js
+++ src/components/IncomeForm.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const IncomeForm = () => {
+
+const IncomeForm = ({ income, setIncome }) => {
+  const desc = useRef(null)
+  const date = useRef(null)
+  const price = useRef(null)
+
+
+  const AddIncome = (e) => {
+    e.preventDefault()
+    
+    let d = date.current.value.split('-')
+    let newD = new Date(d[0], d[1], d[2])
+
+    setIncome([...income, {
+      "desc": desc.current.value,
+      'price': price.current.value,
+      'date': newD.getTime()
+  }])
+  desc.current.value = '';
+  price.current.value = null;
+  date.current.value = null;
+  }
+
+
   return (
-    <form className='income-form' >
+    <form className='income-form' onSubmit={AddIncome} >
       <div className="form-inner">
-        <input type="text" name="desc" id="desc" placeholder='Income Description...' />
-        <input type="number" name="price" id="price" placeholder='Price' />
-        <input type="date" name="date" id="date" placeholder='Income Date' />
+        <input type="text" name="desc" id="desc" placeholder='Income Description...' ref={desc} />
+        <input type="number" name="price" id="price" placeholder='Price' ref={price}/>
+        <input type="date" name="date" id="date" placeholder='Income Date' ref={date} />
         <input type="submit" value="Add Income" />
       </div>
     </form>
---
+++ src/components/IncomeList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IncomeList() {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default IncomeList
