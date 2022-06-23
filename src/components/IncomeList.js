645618e5e05e652d39acadd26267114d466f8514
Pawel <pawel.aleksander@gmail.com>
2022-06-23 16:40:39 +0200

    adding progres

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function IncomeList() {
+function IncomeList({ income, setIncome }) {
   return (
-    <div className=''>
+    <div className='income-list'>
 
     </div>
   )
