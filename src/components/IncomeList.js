6752ca2fd415909f6992a8bf3db6e37051fa01d1
Pawel <pawel.aleksander@gmail.com>
2022-06-23 18:13:59 +0200

    progres

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import IncomeItem from './IncomeItem'
 
 function IncomeList({ income, setIncome }) {
+  const removeIncome = i => {
+let temp = income.filter((v, index) => index !== i);
+setIncome(temp);
+  }
   return (
     <div className='income-list'>
-
+      {
+        income.sort(sortByDate).map((value, index) => (
+          <IncomeItem 
+          key={index} 
+          income={value} 
+          index={index} 
+          removeIncome={removeIncome} 
+          />
+        ))
+      }
     </div>
   )
 }
